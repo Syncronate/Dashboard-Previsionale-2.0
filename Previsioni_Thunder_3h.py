f7bcda0994b6ce5d057170e8bf865405647f47f4
Syncronate <129679946+Syncronate@users.noreply.github.com>
2025-11-26 20:15:35 +0100

    Update Previsioni_Thunder_3h.py

--- Previsioni_Thunder_3h.py
+++ Previsioni_Thunder_3h.py
@@ -174,7 +174,7 @@ class Seq2SeqWithAttention(nn.Module):
 
 # --- Costanti ---
 MODELS_DIR = "models"
-MODEL_BASE_NAME = "modello_seq2seq_20251126_1100_best"
+MODEL_BASE_NAME = "modello_lstm_20251124_1500"
 
 GSHEET_ID = os.environ.get("GSHEET_ID")
 GSHEET_HISTORICAL_DATA_SHEET_NAME = "DATI METEO CON FEATURE"
